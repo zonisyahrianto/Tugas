f2c195418c06fd1d5e58210f9f7ca368c01dcfa3
Zoni Syahrianto <zoni_syahrianto@yahoo.com>
2016-09-12 21:25:35 +0700

    Add files via upload

---
+++ sketch_sep12a.ino
@@ -0,0 +1,36 @@
+//int i=8;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(8, OUTPUT);
+pinMode(9, OUTPUT);
+pinMode(10, OUTPUT);
+pinMode(11, OUTPUT);
+pinMode(A0, OUTPUT);
+pinMode(A1, OUTPUT);
+pinMode(A2, OUTPUT);
+digitalWrite(A0, HIGH);
+digitalWrite(A1, HIGH);
+digitalWrite(A2, HIGH);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for(int x=0;x<16;x++){
+  int i=7;
+while(i<12){
+if(digitalRead(A0)==LOW){
+  i++;
+  digitalWrite(i, HIGH);      
+  delay(500);
+  digitalWrite(i, LOW);      
+  delay(500);
+  }
+   else if(digitalRead(A1)==LOW){
+    i--;
+    digitalWrite(i,HIGH);
+    delay(500);
+  digitalWrite(i, LOW);      
+  delay(500);
+    }}}
+   
+}        
---
+++ sketch_sep12b.ino
@@ -0,0 +1,68 @@
+int ledPin[] ={8,9,10,11};
+int maxCount = 256;
+int delayInterval = 1000;
+
+void setup() {
+  // put your setup code here, to run once:
+  for (int i=8 ; i<12 ; i++)
+  {
+    pinMode(ledPin[i], OUTPUT);
+  }
+digitalWrite(A0, HIGH);
+digitalWrite(A1, HIGH);
+digitalWrite(A2, HIGH);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  //for (int counter = 0; counter < maxCount;counter++)
+  int counter =0;
+while(counter < maxCount){
+  if(digitalRead(A0)==LOW)
+  {
+    counter++;
+    displayBinary(counter);
+    delay(delayInterval);
+    
+  }
+  if(digitalRead(A1)==LOW)
+  {
+    counter--;
+    displayBinaryReverse(counter);
+    delay(delayInterval);
+    
+  }
+  }
+}
+
+void displayBinary(byte numToShow)
+{
+  for (int i=0; i < 8 ;i++)
+  {
+  if (bitRead(numToShow, i)==1)
+  {
+    digitalWrite(ledPin[i], HIGH);
+  }
+    else
+    {
+      digitalWrite(ledPin[i], LOW);
+    }
+}
+}
+
+void displayBinaryReverse(byte numToShow)
+{
+  for (int i=8; i >=0 ;i--)
+  {
+  if (bitRead(numToShow, i)==1)
+  {
+    digitalWrite(ledPin[i], HIGH);
+  }
+    else
+    {
+      digitalWrite(ledPin[i], LOW);
+    }
+}
+}
